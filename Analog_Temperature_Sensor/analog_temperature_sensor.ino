efed8888928c613b99ad2244b01f9e3411a5e747
xnm-sdksdk <n.mendonca@protonmail.com>
2022-12-21 14:53:32 +0000

    new exercises

---
+++ Analog_Temperature_Sensor/analog_temperature_sensor.ino
@@ -0,0 +1,17 @@
+void setup()
+{Serial.begin(9600);
+}
+
+int temp=0;
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+
+int sensorValue = analogRead(A0);
+
+temp=(sensorValue* 0.48828125);
+
+Serial.println(temp);
+ delay(1); }
+} 
---
+++ Stepper_motor_v2/stepper_motor_v2.ino
@@ -0,0 +1,23 @@
+#include <Stepper.h>
+ 
+#define STEPS 100
+ 
+Stepper stepper(STEPS, 8,10,9,11);
+ 
+int previous = 0;
+int direcao=0;
+int val=0;
+ 
+void setup()
+{
+  stepper.setSpeed(180);
+}
+ 
+void loop()
+{
+  val=val+50;
+  if (val<256) direcao=-1;
+  else direcao=1;
+  stepper.step(direcao*10);
+  delay(500);
+}

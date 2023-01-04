e779634a87eda090b7e9859dc6e2298660887997
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-04 16:19:35 +0000

    IR Receive

---
+++ IrReceiver/irReceive.ino
@@ -0,0 +1,35 @@
+#include <IRremote.h>  
+ 
+const int irReceiverPin = 2;  
+const int ledPin = 13;
+ 
+IRrecv irrecv(irReceiverPin); 
+decode_results results;    
+ 
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);   
+  irrecv.enableIRIn();   
+}
+ 
+void loop() 
+{
+  if (irrecv.decode(&results)) {   
+    Serial.print("irCode: ");            
+    Serial.print(results.value, HEX); 
+    Serial.print(",  bits: ");           
+    Serial.println(results.bits); 
+    irrecv.resume();    
+  }  
+  delay(600); 
+  if(results.value == 0xFF6897)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+  }
+}
+
